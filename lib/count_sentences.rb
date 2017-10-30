ed318edde7699037a9dd435f04f3195f9e102957
Amanda Ackerman <ackerm44@gmail.com>
2017-10-30 14:17:57 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     array = []
-    array << self.split(/\.|\!|\?/)
-    puts array.length
+    array << self.split!(/\.|\!|\?/)
+    puts array.s
   end
 end
